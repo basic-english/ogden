bcdd272420258228c046afa6f21094effcaf6399
alxfed <alxfed@gmail.com>
2019-01-09 18:36:29 -0600

    other

---
+++ word_list/qualities50/qualities_50.py
@@ -0,0 +1,10 @@
+"""
+Qualitis 50 words
+"""
+
+# List of 50 opposite qualities
+
+Qualities_50 = ['awake', 'bad', 'bent', 'bitter', 'blue', 'certain', 'cold', 'complete', 'cruel', 'dark', 'dead', 'dear', 'delicate', 'different', 'dirty', 'dry', 'false', 'feeble', 'female', 'foolish', 'future', 'green', 'ill', 'last', 'late', 'left', 'loose', 'loud', 'low', 'mixed', 'narrow', 'old', 'opposite', 'public', 'rough', 'sad', 'safe', 'secret', 'short', 'shut', 'simple', 'slow', 'small', 'soft', 'solid', 'special', 'strange', 'thin', 'white', 'wrong']
+
+print(Qualities_50)
+print(Qualities_50[2])
