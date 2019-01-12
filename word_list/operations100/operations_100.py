7bb81dcc42d232195f41afe2b296009d2366c80f
alxfed <alxfed@gmail.com>
2019-01-12 08:32:37 -0600

    line breaks

--- word_list/operations100/operations_100.py
+++ word_list/operations100/operations_100.py
@@ -3,24 +3,39 @@ Writing Ogden's "Operations" as a list into a .json file
 """
 
 # Tuples
-OPERATIONS_1 = ('come', 'get', 'give', 'go', 'keep', 'let', 'make', 'put', 'seem', 'take', 'be', 'do', 'have', 'say', 'see', 'send', 'may', 'will')
-OPERATIONS_2 = ('about', 'across', 'after', 'against', 'among', 'at', 'before', 'between', 'by', 'down', 'from', 'in', 'off', 'on', 'over', 'through', 'to', 'under', 'up', 'with')
+OPERATIONS_1 = ('come', 'get', 'give', 'go', 'keep', 'let', 'make', 'put',
+                'seem', 'take', 'be', 'do', 'have', 'say', 'see', 'send',
+                'may', 'will')
+OPERATIONS_2 = ('about', 'across', 'after', 'against', 'among', 'at', 'before',
+                'between', 'by', 'down', 'from', 'in', 'off', 'on', 'over',
+                'through', 'to', 'under', 'up', 'with')
 OPERATIONS_3 = ('as', 'for', 'of', 'till', 'than')
-OPERATIONS_4 = ('a', 'the', 'all', 'any', 'every', 'little', 'much', 'no', 'other', 'some', 'such', 'that', 'this', 'I ', 'he', 'you', 'who')
-OPERATIONS_5 = ('and', 'because', 'but', 'or', 'if', 'though', 'while', 'how', 'when', 'where', 'why')
-OPERATIONS_6 = ('again', 'ever', 'far', 'forward', 'here', 'near', 'now', 'out', 'still', 'then', 'there', 'together', 'well')
-OPERATIONS_7 = ('almost', 'enough', 'even', 'not', 'only', 'quite', 'so', 'very', 'tomorrow', 'yesterday')
+OPERATIONS_4 = ('a', 'the', 'all', 'any', 'every', 'little', 'much', 'no',
+                'other', 'some', 'such', 'that', 'this', 'I ', 'he', 'you', 'who')
+OPERATIONS_5 = ('and', 'because', 'but', 'or', 'if', 'though', 'while', 'how',
+                'when', 'where', 'why')
+OPERATIONS_6 = ('again', 'ever', 'far', 'forward', 'here', 'near', 'now', 'out',
+                'still', 'then', 'there', 'together', 'well')
+OPERATIONS_7 = ('almost', 'enough', 'even', 'not', 'only', 'quite', 'so', 'very',
+                'tomorrow', 'yesterday')
 OPERATIONS_8 = ('north', 'south', 'east', 'west', 'please', 'yes')
 OPERATIONS = (OPERATIONS_1, OPERATIONS_2, OPERATIONS_3, OPERATIONS_4, OPERATIONS_5, OPERATIONS_6, OPERATIONS_7, OPERATIONS_8)
 
 # Lists
-Operations_1 = ['come', 'get', 'give', 'go', 'keep', 'let', 'make', 'put', 'seem', 'take', 'be', 'do', 'have', 'say', 'see', 'send', 'may', 'will']
-Operations_2 = ['about', 'across', 'after', 'against', 'among', 'at', 'before', 'between', 'by', 'down', 'from', 'in', 'off', 'on', 'over', 'through', 'to', 'under', 'up', 'with']
+Operations_1 = ['come', 'get', 'give', 'go', 'keep', 'let', 'make', 'put', 'seem',
+                'take', 'be', 'do', 'have', 'say', 'see', 'send', 'may', 'will']
+Operations_2 = ['about', 'across', 'after', 'against', 'among', 'at', 'before',
+                'between', 'by', 'down', 'from', 'in', 'off', 'on', 'over',
+                'through', 'to', 'under', 'up', 'with']
 Operations_3 = ['as', 'for', 'of', 'till', 'than']
-Operations_4 = ['a', 'the', 'all', 'any', 'every', 'little', 'much', 'no', 'other', 'some', 'such', 'that', 'this', 'I ', 'he', 'you', 'who']
-Operations_5 = ['and', 'because', 'but', 'or', 'if', 'though', 'while', 'how', 'when', 'where', 'why']
-Operations_6 = ['again', 'ever', 'far', 'forward', 'here', 'near', 'now', 'out', 'still', 'then', 'there', 'together', 'well']
-Operations_7 = ['almost', 'enough', 'even', 'not', 'only', 'quite', 'so', 'very', 'tomorrow', 'yesterday']
+Operations_4 = ['a', 'the', 'all', 'any', 'every', 'little', 'much', 'no', 'other',
+                'some', 'such', 'that', 'this', 'I ', 'he', 'you', 'who']
+Operations_5 = ['and', 'because', 'but', 'or', 'if', 'though', 'while', 'how',
+                'when', 'where', 'why']
+Operations_6 = ['again', 'ever', 'far', 'forward', 'here', 'near', 'now', 'out',
+                'still', 'then', 'there', 'together', 'well']
+Operations_7 = ['almost', 'enough', 'even', 'not', 'only', 'quite', 'so', 'very',
+                'tomorrow', 'yesterday']
 Operations_8 = ['north', 'south', 'east', 'west', 'please', 'yes']
 Operations = [Operations_1, Operations_2, Operations_3, Operations_4, Operations_5, Operations_6, Operations_7, Operations_8]
 
