67fb5e000d1e6bda1bad9ca196e8d67671c8a12c
alxfed <alxfed@gmail.com>
2019-01-18 16:47:31 -0600

    correction

---
+++ word_list/verbs16/verbs_16.py
@@ -0,0 +1,15 @@
+"""
+16 verbs plus 'seem'
+"""
+
+# Lists
+Verbs_16 = ['cause', 'come', 'get', 'give',
+           'go', 'keep', 'let', 'make',
+           'put', 'seem', 'take', 'be',
+           'do', 'have', 'say', 'see', 'send']
+Verbs_plus = ['seem']
+Verbs = Verbs_16 + Verbs_plus
+
+"""
+print(Verbs)
+"""
